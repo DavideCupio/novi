72dc6543530cc549edb632e2309ba4b4824df998
DavideCupio <68639446+DavideCupio@users.noreply.github.com>
2025-07-25 18:56:08 +0200

    Create header.php

---
+++ patterns/header.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: Header
+ * Slug: novi/header
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <!-- wp:group {"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+        <!-- wp:heading -->
+        <h2 class="wp-block-heading">Header</h2>
+        <!-- /wp:heading -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
