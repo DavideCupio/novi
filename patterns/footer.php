71db2dde86a872be52f197545398f5274773ad0f
DavideCupio <68639446+DavideCupio@users.noreply.github.com>
2025-07-25 18:56:12 +0200

    Create footer.php

---
+++ patterns/footer.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: Footer
+ * Slug: novi/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <!-- wp:group {"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+        <!-- wp:heading -->
+        <h2 class="wp-block-heading">Footer</h2>
+        <!-- /wp:heading -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
