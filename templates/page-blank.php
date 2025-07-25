f65bc97436b0b332cca499f0814bbcdb9dae0485
DavideCupio <68639446+DavideCupio@users.noreply.github.com>
2025-07-25 20:42:47 +0200

    Create page-blank.php

---
+++ templates/page-blank.php
@@ -0,0 +1,20 @@
+<?php
+//Template Name: Blank Page
+//Description: A blank page template without header and footer.
+?>
+<main id="main-content" role="main" <?php post_class('site-main'); ?>>
+    <div class="is-layout-constrained has-global-padding">
+        <div class="page-content container">
+
+            <?php
+            while (have_posts()) :
+                the_post();
+                echo '<div class="is-layout-constrained has-global-padding">';
+                the_content();
+                echo '</div>';
+            endwhile;
+            ?>
+
+        </div>
+    </div>
+</main>
